a24ee3d0b3b6ab832820a0e2f6fca76ac6d764a4
Evgeniy <gekasan4@yandex.ru>
2023-03-02 00:39:07 +0300

    main-svc

--- main-service/src/main/java/ru/practicum/evm/config/StatsClientConfig.java
+++ main-service/src/main/java/ru/practicum/evm/config/StatsClientConfig.java
@@ -1,6 +1,5 @@
 package ru.practicum.evm.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +7,10 @@ import ru.practicum.statsclient.StatsClient;
 
 @Configuration
 public class StatsClientConfig {
-    @Value("${stats-service.url}")
-    private String url;
 
     @Bean("StatClient")
     StatsClient statsClient() {
         RestTemplateBuilder builder = new RestTemplateBuilder();
-        return new StatsClient(url, builder);
+        return new StatsClient("http://localhost:9090", builder);
     }
 }
